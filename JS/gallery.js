6f790c3ad31826684cb3767b76570b82d1273d85
Aman <amanmalviya14032000@gmail.com>
2021-12-06 00:01:14 +0530

    Code refactored

---
+++ JS/gallery.js
@@ -0,0 +1,114 @@
+
+let description1 = document.getElementById('disc1');
+let description2 = document.getElementById('disc2');
+let description3 = document.getElementById('disc3');
+let description4 = document.getElementById('disc4');
+let description5 = document.getElementById('disc5');
+let description6 = document.getElementById('disc6');
+let description7 = document.getElementById('disc7');
+let description8 = document.getElementById('disc8');
+let description9 = document.getElementById('disc9');
+
+
+let check1 = document.getElementById('item1');
+let check2 = document.getElementById('item2');
+let check3 = document.getElementById('item3');
+let check4 = document.getElementById('item4');
+let check5 = document.getElementById('item5');
+let check6 = document.getElementById('item6');
+let check7 = document.getElementById('item7');
+let check8 = document.getElementById('item8');
+let check9 = document.getElementById('item9');
+
+
+
+
+function showHide1() {
+    if (!check1.checked) {
+        description1.style.visibility = 'visible';
+        description1.style.opacity = '1';
+    } else {
+        description1.style.visibility = 'hidden';
+        description1.style.opacity = '0';
+    } 
+}
+
+function showHide2() {
+    if (!check2.checked) {
+        description2.style.visibility = 'visible';
+        description2.style.opacity = '1';
+    } else {
+        description2.style.visibility = 'hidden';
+        description2.style.opacity = '0';
+    } 
+}
+
+function showHide3() {
+    if (!check3.checked) {
+        description3.style.visibility = 'visible';
+        description3.style.opacity = '1';
+    } else {
+        description3.style.visibility = 'hidden';
+        description3.style.opacity = '0';
+    } 
+}
+
+function showHide4() {
+    if (!check4.checked) {
+        description4.style.visibility = 'visible';
+        description4.style.opacity = '1';
+    } else {
+        description4.style.visibility = 'hidden';
+        description4.style.opacity = '0';
+    } 
+}
+
+function showHide5() {
+    if (!check5.checked) {
+        description5.style.visibility = 'visible';
+        description5.style.opacity = '1';
+    } else {
+        description5.style.visibility = 'hidden';
+        description5.style.opacity = '0';
+    } 
+}
+
+function showHide6() {
+    if (!check6.checked) {
+        description6.style.visibility = 'visible';
+        description6.style.opacity = '1';
+    } else {
+        description6.style.visibility = 'hidden';
+        description6.style.opacity = '0';
+    } 
+}
+
+function showHide7() {
+    if (!check7.checked) {
+        description7.style.visibility = 'visible';
+        description7.style.opacity = '1';
+    } else {
+        description7.style.visibility = 'hidden';
+        description7.style.opacity = '0';
+    } 
+}
+
+function showHide8() {
+    if (!check8.checked) {
+        description8.style.visibility = 'visible';
+        description8.style.opacity = '1';
+    } else {
+        description8.style.visibility = 'hidden';
+        description8.style.opacity = '0';
+    } 
+}
+
+function showHide9() {
+    if (!check9.checked) {
+        description9.style.visibility = 'visible';
+        description9.style.opacity = '1';
+    } else {
+        description9.style.visibility = 'hidden';
+        description9.style.opacity = '0';
+    } 
+}
